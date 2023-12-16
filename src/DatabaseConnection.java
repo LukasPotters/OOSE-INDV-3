bb0a705f27bce975c2c73af95c834e7ebb7143b5
Lukas Potters <lukas@vasio.nl>
2023-12-16 16:14:31 +0100

    database connection manager

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,8 +1,8 @@
 import java.sql.*;
 
-public class DatabaseManager {
+public class DatabaseConnection {
     private Connection connection;
-    public DatabaseManager() {
+    public DatabaseConnection() {
         String DB_URL = "jdbc:mysql://localhost:3306/test_db";
         String DB_USER = "root";
         String DB_PASSWORD = "griqdfaz83";
@@ -22,19 +22,6 @@ public class DatabaseManager {
         }
     }
 
-    public ResultSet executeQuery(String query, Object... params) {
-        try {
-            PreparedStatement statement = connection.prepareStatement(query);
-            for (int i = 0; i < params.length; i++) {
-                statement.setObject(i+1, params[i]);
-            }
-            return statement.executeQuery();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Failed to execute query.");
-        }
-    }
-
     public int executeUpdate(String query, Object... params) {
         try {
             PreparedStatement statement = connection.prepareStatement(query);
@@ -45,6 +32,33 @@ public class DatabaseManager {
         } catch (SQLException e) {
             e.printStackTrace();
             throw new RuntimeException("Failed to execute update.");
+        }
+    }
+
+
+    public ResultSet queryItem(String tableName, String fieldName, String fieldValue) {
+        String query = "SELECT * FROM ? WHERE ? = ?";
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, tableName);
+            statement.setString(2, fieldName);
+            statement.setString(3, fieldValue);
+            return statement.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to execute query.");
+        }
+    }
+
+    public ResultSet queryItems(String tableName) {
+        String query = "SELECT * FROM ?";
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, tableName);
+            return statement.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to execute query.");
         }
     }
 
